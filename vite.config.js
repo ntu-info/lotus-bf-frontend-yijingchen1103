6c72b0121c1cbac559e4b32b1cc83d27dbe5be73
yijingchen1103 <yjchen11103@gmail.com>
2025-11-06 22:14:33 +0800

    HW

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,23 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/lotus-bf-frontend-yijingchen1103/',  // ✅ 必須加上 repo 名稱！
+  // 一樣要對應 repo 名稱
+  base: '/lotus-bf-frontend-yijingchen1103/',
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+
+  // 讓 build 輸出到 docs/
+  build: {
+    outDir: 'docs'
+  },
+
+  // 本機開發用的設定，保持原樣即可
   server: {
     allowedHosts: ['mil.psy.ntu.edu.tw']
   }
